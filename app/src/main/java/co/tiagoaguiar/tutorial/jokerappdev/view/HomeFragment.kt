eae5772733a60f2280007fd813f28571f333855d
Thiago-S-Brito <thhiago369@gmail.com>
2024-12-11 17:48:33 -0300

    segundo commit, 40% do projeto completo

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/HomeFragment.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/HomeFragment.kt
@@ -5,15 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.tutorial.jokerappdev.R
+import co.tiagoaguiar.tutorial.jokerappdev.modeel.Category
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieAdapter
 
 class HomeFragment : Fragment () {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home,container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_main)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val adapter = GroupieAdapter()
+        recyclerView.adapter = adapter
+
+        adapter.add(CategoryItem(Category("categoria 1", 0xffface6e)))
+        adapter.add(CategoryItem(Category("categoria 2", 0xffecd16d)))
+        adapter.add(CategoryItem(Category("categoria 3", 0xffded36e)))
+        adapter.add(CategoryItem(Category("categoria 4", 0xffcfd571)))
+
+        adapter.notifyDataSetChanged()
     }
 }
